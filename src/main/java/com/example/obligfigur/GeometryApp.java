76263df1b86cc336cb674b88fe11d7099425941f
IamBananana <richardgjelland@hotmail.com>
2024-09-23 19:58:54 +0200

    changed file name

--- src/main/java/com/example/obligfigur/GeometryApp.java
+++ src/main/java/com/example/obligfigur/GeometryApp.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GeometryApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane borderPane = new BorderPane();
