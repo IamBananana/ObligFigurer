6938ac0262c5c8bed1a93ca4e4c113b340fef168
IamBananana <richardgjelland@hotmail.com>
2024-09-23 19:23:45 +0200

    deleted shit button

--- src/main/java/com/example/obligfigur/HelloApplication.java
+++ src/main/java/com/example/obligfigur/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.obligfigur;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
