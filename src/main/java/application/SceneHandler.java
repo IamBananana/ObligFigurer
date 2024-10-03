8995c1c3d83a8455403990866162817d4117f9b2
sUmi7ro <simonto.no@outlook.com>
2024-10-03 11:04:09 +0200

    dpioawj

--- src/main/java/application/SceneHandler.java
+++ src/main/java/application/SceneHandler.java
@@ -19,7 +19,7 @@ public class SceneHandler {
         this.scene = scene;
         intialize();
     }
-
+    ///dlkwnkdlajjdwajddiawjdp
     private void intialize() {
         BorderPane bp = (BorderPane) scene.getRoot();
 
